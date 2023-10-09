474d49b9757e3fd2a740b1c3f194d8d044c75773
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-10-09 18:29:50 -0300

    Vista en el home

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) =>{
+    res.send('Gatooooo');
+})
+
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
 });
